d6710c89c44658e62f0ed11ef0d3139b9f7bb709
Kayla Youhanaie <kaylayouhanaie2027@u.northwestern.edu>
2026-04-17 11:35:54 -0500

    IMU data readings working

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -1,11 +1,9 @@
-// ssd1306.c merged with this file
-//based on adafruit and sparkfun libraries
 #include <stdio.h>
-#include <string.h> // for memset
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
 #include "ssd1306.h"
 #include "font.h"
-#include "hardware/i2c.h"
-#include "pico/stdlib.h"
 #include "hardware/adc.h"
 
 // I2C defines
@@ -14,13 +12,41 @@
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
-#define chip_address 0x20
+#define IMU_ADDRESS 0x68
 #define IODIR 0x00
 #define OLAT 0x0A
 #define GPIO 0x09
 
-unsigned char SSD1306_ADDRESS = 0b0111100; // 7bit i2c address
-unsigned char ssd1306_buffer[513]; // 128x32/8. Every bit is a pixel except first byte
+// config registers
+#define CONFIG 0x1A
+#define GYRO_CONFIG 0x1B
+#define ACCEL_CONFIG 0x1C
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+// sensor data registers:
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_XOUT_L 0x3C
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_YOUT_L 0x3E
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
+#define TEMP_OUT_H   0x41
+#define TEMP_OUT_L   0x42
+#define GYRO_XOUT_H  0x43
+#define GYRO_XOUT_L  0x44
+#define GYRO_YOUT_H  0x45
+#define GYRO_YOUT_L  0x46
+#define GYRO_ZOUT_H  0x47
+#define GYRO_ZOUT_L  0x48
+#define WHO_AM_I     0x75
+
+
+unsigned char SSD1306_ADDRESS = 0b0111100;
+unsigned char ssd1306_buffer[513];
+unsigned char readPin(unsigned char reg);
+void setPin(unsigned char reg, unsigned char value);
+void readAllIMU(unsigned char reg, unsigned char *buf);
+int16_t convertData(unsigned char byte1, unsigned char byte2);
 
 void drawLetter(unsigned char x, unsigned char y, unsigned char letter);
 void drawPixel(unsigned char x, unsigned char y, unsigned char color);
@@ -29,6 +55,7 @@ void drawMessage(unsigned char x, unsigned char y, char *m);
 int main()
 {
     stdio_init_all();
+    sleep_ms(1000);
 
     const uint LED_PIN = 16;
     gpio_init(LED_PIN);
@@ -41,67 +68,116 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    // call ssd setup, clear, update here
     ssd1306_setup();
     ssd1306_clear();
     ssd1306_update();
 
-    while (true) {
-        // heartbeat LED
+    // check IMU WHO_AM_I register
+    uint8_t me = readPin(WHO_AM_I);
+    char message_me[50]; 
+    sprintf(message_me, "%d", me); 
+    drawMessage(0,0,message_me);
+
+
+    // initialize IMU
+    setPin(PWR_MGMT_1,0x00);
+    setPin(ACCEL_CONFIG,0b00000000);
+    setPin(GYRO_CONFIG,0b00011000);
+
+    
+
+    while (true){
         gpio_put(LED_PIN, 1);
         sleep_ms(500);
         gpio_put(LED_PIN, 0);
         sleep_ms(500);
-        //
-        
-        unsigned int t1 = to_us_since_boot(get_absolute_time()); // time at beginning
+        printf("kayla\n");
+        sleep_ms(100);
 
-        // drawLetter example
-        // unsigned char my_letter1 = 'K';
-        // unsigned char my_letter2 = 'Y';
-        // drawLetter(0,0,my_letter1);
-        // drawLetter(7,0,my_letter2);
+        //read raw data
+        unsigned char allRawData[14];
+        readAllIMU(0x3B,allRawData);
+        for (int i = 0; i < 14; i++) {
+        printf("Reading %d: %d\r\n", i, allRawData[i]);
+        }
 
-        char message1[50]; 
-        sprintf(message1,"kaylakaylakaylakaylakayla");
-        drawMessage(0,0,message1);
+        // convert data to 16 bit integers
+        float new_data[7];
+        int index_count = 0;
+        for (int i=0; i<14; i+=2){
+            new_data[index_count] = convertData(allRawData[i], allRawData[i+1]);
 
-        char message2[50]; 
-        sprintf(message2,"kaylakaylakaylakaylakayla");
-        drawMessage(0,8,message2);
+            if (i<6){
+                new_data[index_count] *= 0.000061; // convert accels to units of g
+            }
+            if (i>5 && i<8){
+                new_data[index_count] = new_data[index_count]/340.0 + 36.53; // convert temp to units C
+            }
+            if (i>=8){
+                new_data[index_count] *= 0.007630; // convert gyro to units of deg/sec
+            }
+            index_count++;
+        }
 
-        char message3[50]; 
-        sprintf(message3,"kaylakaylakaylakaylakayla");
-        drawMessage(0,16,message3);
+        for (int i = 0; i < 7; i++) {
+            printf("New Data %d: %f\r\n", i, new_data[i]);
+        }
 
-        // char message4[50]; 
-        // sprintf(message4,"kaylakay");
-        // drawMessage(0,24,message4);
-
-        //read adc0
-        adc_init();
-        adc_gpio_init(26);
-        adc_select_input(0);
-        uint8_t read_adc0 = adc_read();
-
-        // display adc0 value
-        char message_adc[50]; 
-        sprintf(message_adc, "ADC0=%d", read_adc0); 
-        drawMessage(0,24,message_adc);
-
-        // find and display fps
-        unsigned int t2 = to_us_since_boot(get_absolute_time()); // time at end
-        float runtime = (float)(t2 - t1)/1000000; // convert microseconds to seconds
-        float fps = 1/runtime;
-        char message_time[50]; 
-        sprintf(message_time, "FPS=%f", fps); 
-        drawMessage(50,24,message_time);
-
+        // unsigned char my_letter = 'Y';
+        // if (new_data[1] < 10){
+        //     drawLetter(30,30,my_letter);
+        // }
         ssd1306_update();
     }
+    
+
+    // int16_t accel = new_data[2];
+    // char message_accel[50]; 
+    // sprintf(message_accel, "%d", accel); 
+    // drawMessage(0,30,message_accel);
+
+
 }
+
+
+void readAllIMU(unsigned char reg, unsigned char *buf){
+    i2c_write_blocking(I2C_PORT, IMU_ADDRESS, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, IMU_ADDRESS, buf, 14, false);
+}
+
+
+unsigned char readPin(unsigned char reg){
+    unsigned char buf;
+    i2c_write_blocking(I2C_PORT, IMU_ADDRESS, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, IMU_ADDRESS, &buf, 1, false);
+    return buf;
+}
+
+void setPin(unsigned char reg, unsigned char value){
+    uint8_t buf[2];
+    buf[0] = reg;
+    buf[1] = value;
+    i2c_write_blocking(I2C_PORT, IMU_ADDRESS, buf, 2, false);
+}
+
+int16_t convertData(unsigned char byte1, unsigned char byte2){
+    int16_t converted_data = (byte1 << 8) | byte2;
+    return converted_data;
+}
+
+
+/*
+April 14th Lecture notes
+look at datasheet to find bits to change for chihp initialization three registers
+
+find which way is down by subtracting gyroscope from accelerometer
+
+only care about x and y acceleration data
+
+*/ 
+
+
 
 
 
@@ -224,5 +300,3 @@ void ssd1306_clear() {
     memset(ssd1306_buffer, 0, 512); // make every bit a 0, memset in string.h
     ssd1306_buffer[0] = 0x40; // first byte is part of command
 }
-
-
